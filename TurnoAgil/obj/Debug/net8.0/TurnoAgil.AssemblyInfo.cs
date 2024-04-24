594948177dc165285c71d53b6219b255102b1bce
juangiraldo95 <juanfe-0701@hotmail.com>
2024-04-24 14:49:17 -0500

    commit desps de bajar mi rama

--- TurnoAgil/obj/Debug/net8.0/TurnoAgil.AssemblyInfo.cs
+++ TurnoAgil/obj/Debug/net8.0/TurnoAgil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnoAgil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7e9f0aa1888083150ee1a7b3aa96c75743f5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnoAgil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnoAgil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
